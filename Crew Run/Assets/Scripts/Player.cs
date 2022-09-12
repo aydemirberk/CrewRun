f4cb0211563f8eb4747acd2d0bc6d3e5293a8d43
Berk Aydemir <beerkaydemir@gmail.com>
2022-09-12 20:55:26 +0300

    Created Script For Player
    Added movement for player

---
+++ Crew Run/Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private int _playerSpeed = 1;
+
+    private void FixedUpdate()
+    {
+        transform.Translate(Vector3.forward * _playerSpeed * Time.deltaTime);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            if (Input.GetAxis("Mouse X") < 0)
+            {
+                transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x-.1f, transform.position.y, transform.position.z), .3f);
+            }
+
+            if(Input.GetAxis("Mouse X")> 0)
+            {
+                transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x + .1f, transform.position.y, transform.position.z), .3f);
+            }
+        }
+
+
+    }
+}
