112dd0c1bb7508c037ac9ad7a2c6b8beff45cdd1
Berk Aydemir <beerkaydemir@gmail.com>
2022-09-15 00:45:08 +0300

    Added On Trigger Method for Sub Characters

---
+++ Crew Run/Assets/Scripts/SubCharacter.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SubCharacter : MonoBehaviour
+{
+    GameObject Target;
+    NavMeshAgent _Navmesh;
+
+
+    void Start()
+    {
+        _Navmesh = GetComponent<NavMeshAgent>();
+        Target = GameObject.FindWithTag("GameManager").GetComponent<GameManager>().TargetPoint;
+    }
+
+    private void LateUpdate()
+    {
+        _Navmesh.SetDestination(Target.transform.position);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("BadObject"))
+        {
+            GameObject.FindWithTag("GameManager").GetComponent<GameManager>().characterCounter--;
+            gameObject.SetActive(false);
+        }
+    }
+}
